6d6d4b5c6b5d825b23f7af85afe699a92db93cd9
zm1105 <827556302@qq.com>
2019-08-12 19:32:44 +0800

    求和优化

--- src/main/java/TestDemo03.java
+++ src/main/java/TestDemo03.java
@@ -3,18 +3,22 @@ import java.util.List;
 
 public class TestDemo03 {
     public static void main(String[] args) {
+        getnum(100);
+    }
+
+    private static void getnum(Integer integer) {
         List list = new LinkedList();
         List list2 = new LinkedList();
-        for (int i = 0; i <= 100; i++) {
+        for (int i = 0; i <= integer; i++) {
             if (i % 2 != 0) {
                 list.add(i);
             } else if (i % 2 == 0) {
                 list2.add(i);
             }
         }
-        System.out.println("1到100的奇数是");
+        System.out.println("1到" + integer + "的奇数是");
         ListTest.getForList(list);
-        System.out.println("1到100的偶数是");
+        System.out.println("1到" + integer + "的偶数是");
         ListTest.getForList(list2);
     }
 }
